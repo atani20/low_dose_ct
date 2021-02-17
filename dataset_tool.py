e392787625595982a1ba7831e45bc0c0aecd568e
Tatiana <loloool@mail.ru>
2021-02-17 23:13:53 +0300

    Add progress bar and asserts

--- dataset_tool.py
+++ dataset_tool.py
@@ -1,12 +1,14 @@
 import os
-from pydicom import dcmread
 import numpy as np
 import h5py
 import config
-import random
+from pydicom import dcmread
+from progress.bar import IncrementalBar
 
 
 def get_patient_pixels(patient_path):
+    # load list of slices and extract pixel array
+    assert os.path.isdir(patient_path)
     slices = [dcmread(os.path.join(patient_path, s)) for s in os.listdir(patient_path)]
     image = np.stack([s.pixel_array for s in slices])
     image = image.astype(np.int16)
@@ -19,7 +21,11 @@ def get_patient_pixels(patient_path):
 
 
 def extract_patches(full_images, low_images, patch_size, alpha=0.5, patient_shuffle=True, patch_shuffle=True):
+    assert 0 <= alpha <= 1
+    assert full_images.shape == low_images.shape
+    # cut images into pathes
     images_num, size, size1 = low_images.shape
+    assert size >= patch_size
     full_out = np.empty((0, patch_size, patch_size), dtype='int16')
     low_out = np.empty((0, patch_size, patch_size), dtype='int16')
     images_idxs = np.random.permutation(np.arange(images_num)) if patient_shuffle else np.arange(images_num)
@@ -33,11 +39,13 @@ def extract_patches(full_images, low_images, patch_size, alpha=0.5, patient_shuf
 
         patch_idxs1 = np.random.permutation(np.arange(n)) if patch_shuffle else np.arange(n)
         patch_idxs2 = np.random.permutation(np.arange(n)) if patch_shuffle else np.arange(n)
+        # cut one image into pathes
         for i in patch_idxs1:
             for j in patch_idxs2:
                 full_patch = full_img[i*patch_size: (i+1)*patch_size, j*patch_size: (j+1)*patch_size]
+                # if pach is black (all pixels less than 250) than add it to dataset with probability alpha
                 if np.all(full_patch <= 250):
-                    if np.random.uniform(0, 1) < alpha:
+                    if np.random.uniform(0, 1) > alpha:
                         k += 1
                         continue
                 full_patches = np.concatenate((full_patches, np.reshape(full_patch, (1,) + full_patch.shape)))
@@ -47,13 +55,15 @@ def extract_patches(full_images, low_images, patch_size, alpha=0.5, patient_shuf
                 
         full_out = np.concatenate((full_out, full_patches))
         low_out = np.concatenate((low_out, low_patches))
-    print(k)
     return full_out, low_out
 
 
-# создание датасета, режем на кусочки размера patch_size
 def dataset_prerprocessing(path, patch_size):
-    for patient in os.listdir(path):
+    assert os.path.isdir(path)
+    patient_list = os.listdir(path)
+    bar = IncrementalBar('Patient', max=len(patient_list))
+    for patient in patient_list:
+        bar.next()
         doses = os.listdir(os.path.join(path, patient))
         full_pixels = get_patient_pixels(os.path.join(path, patient, doses[0]))
         low_pixels = get_patient_pixels(os.path.join(path, patient, doses[1]))
@@ -64,6 +74,7 @@ def dataset_prerprocessing(path, patch_size):
             f.create_dataset('full', data=low_patches)
         del full_patches
         del low_patches
+    bar.finish()
 
 
 if __name__ == "__main__":
